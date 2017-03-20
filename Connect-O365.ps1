1626d24fa6ce8919e85d2b32cda3eddf879ec194
paratz <pablo@ratz.com.ar>
2017-03-20 11:13:30 -0300

    upload
    upload

---
+++ Connect-O365.ps1
@@ -0,0 +1,10 @@
+﻿$UserCredential = Get-Credential
+Connect-MsolService -Credential $UserCredential
+
+Set-ExecutionPolicy RemoteSigned
+
+$SessionEx = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $SessionEx
+
+$sessionSfB = New-CsOnlineSession -Credential $UserCredential -Verbose
+Import-PSSession $sessionSfB

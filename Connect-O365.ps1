08b6fafeb83249d8464941d4726bef5c6bf070fa
paratz <pablo@ratz.com.ar>
2017-04-13 17:26:16 -0300

    1

--- Connect-O365.ps1
+++ Connect-O365.ps1
@@ -1,7 +1,7 @@
 ﻿$UserCredential = Get-Credential
 Connect-MsolService -Credential $UserCredential
 
-Set-ExecutionPolicy RemoteSigned
+#Set-ExecutionPolicy RemoteSigned
 
 $SessionEx = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 Import-PSSession $SessionEx
---
+++ Disable-O365DirSync.ps1
@@ -0,0 +1,3 @@
+﻿Set-MsolDirSyncEnabled -EnableDirSync $false
+
+#https://support.office.com/en-us/article/Turn-off-directory-synchronization-for-Office-365-ee5f861e-bd48-4267-83d1-a4ead4b4a00d
